02aeed5c4541fe648b2da8696c5459ab945694b2
Aiste Mackeviciute <a.mackeviciute.21@gmail.com>
2022-03-26 09:54:12 +0200

    init

---
+++ src/test/java/com/dokobit/compressor/CompressorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokobit.compressor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompressorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
